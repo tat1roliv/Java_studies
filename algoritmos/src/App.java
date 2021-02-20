ff5fef0a874f02509d0d20970dcbaa4fd8399dbe
tati oliveira <tat1roliv@gmail.com>
2021-02-20 17:38:20 -0300

    app.java

---
+++ algoritmos/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        numero = 10*2;
+        System.out.println("hola"+numero);
+    }
+}
