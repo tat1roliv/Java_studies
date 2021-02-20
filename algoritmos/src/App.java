a57d1195cefa2193b615ec0ffe5a5dac7edc3126
tati oliveira <tat1roliv@gmail.com>
2021-02-20 18:08:25 -0300

    Update App.java

--- algoritmos/src/App.java
+++ algoritmos/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         int numero = 10;
         numero = 10*2;
-        System.out.println("hola"+numero);
+        System.out.println("hola" + numero);
     }
 }
