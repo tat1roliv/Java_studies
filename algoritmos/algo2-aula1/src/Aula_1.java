88decea0b027311a204681d77a9c06e5ef605dc9
tati oliveira <tat1roliv@gmail.com>
2021-02-21 15:55:46 -0300

    algo2_aula1

---
+++ algoritmos/algo2-aula1/src/Aula_1.java
@@ -0,0 +1,29 @@
+
+public class Aula_1 {
+    public static void main(String[] args)  {
+        System.out.println("aula 1");
+    }
+}
+//tipos de dados primitivos
+/*
+boolean -> t || f
+char -> 1 carac
+short -> inteiro de 16 bits
+int -> inteiro de 32 bits
+long -> inteiro de 64 bits;
+float -> real de 32 bits;
+double -> real de 64 bits;
+
+String -> caracteres (classe em java)
+*/
+
+//declaracao var
+/*
+int pesoVeiculo;
+float precoProduto;
+byte idadeAluno;
+char sexoFuncionario;
+String nome_Aluno;
+boolean maiorIdade;
+*/
+
--- algoritmos/algo2-aula1/src/exemplo.java
+++
@@ -1,7 +0,0 @@
-
-public class exemplo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("aula 1");
-    }
-}
-//min 30
