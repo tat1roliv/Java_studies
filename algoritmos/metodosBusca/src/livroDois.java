1948abbef5807c00a64c5a83b8df0af905ef1d93
tati oliveira <tat1roliv@gmail.com>
2021-03-01 19:00:52 -0300

    Create livroDois.java

---
+++ algoritmos/metodosBusca/src/livroDois.java
@@ -0,0 +1,5 @@
+//2. Crie um vetor ordenado com aproximadamente mil números inteiros gerados aleatoriamente. Depois, utilize a técnica da busca sequencial indexada para otimizar o processo da busca. Sugestão, utilize uma tabela índice com um intervalo de cem chaves, aproximadamente.
+
+public class livroDois {
+    
+}
