2a75856dd86aed206c4860da12db1cdb2fc94226
tati oliveira <tat1roliv@gmail.com>
2021-03-01 18:56:53 -0300

    update

---
+++ algoritmos/metodosBusca/src/livroUm.java
@@ -0,0 +1,11 @@
+// /No vetor [2, 5, 7, 9, 10, 8, 92] é possível aplicar a busca binária?
+//nao, por nao estar ordenado
+//step1 ordenar
+//step2 busca binaria
+
+public class livroUm {
+    public static void main(String[] args) {
+        int v[] = {2,5,7,9,10,8,92};
+        
+    }
+}
