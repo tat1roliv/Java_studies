fb8ff6827ab7ec731d126217430f4b7cb1aee5b7
tati oliveira <tat1roliv@gmail.com>
2021-03-06 16:19:17 -0300

    Sorting
    BubbleSort;
    InsertionSort;
    SelectionSort;

---
+++ algoritmos2/src/aulas/Web4_BubbleSort.java
@@ -0,0 +1,47 @@
+package aulas;
+import java.util.Arrays;
+
+/*
+BubbleSort
+compara pares de elementos subjacentes (elemento da esquerda com o seguinte, se maior, troca;
+"borbulhando", joga os maiores elementos para as posicoes finais do vetor
+
+https://www.youtube.com/watch?v=Cq7SMsQBEUw
+
+*/
+
+
+public class Web4_BubbleSort {
+
+
+	    public static void main(String[] args) {
+
+	        int[] v= { 32, 17, 51, 8, 23 };
+
+	        System.out.printf("inicio -> %s \n", Arrays.toString(v));
+
+	        bubble(v);
+
+	        System.out.printf("fim -> %s \n", Arrays.toString(v));
+
+	    }
+	        
+	    public static void bubble(int[] v) {
+	        for (int i=0; i < v.length; i++){
+	            for (int j=0; j < v.length - 1; j++){
+	                if (v[j] > v[j + 1]){
+	                    int aux = v[j];
+	                    v[j] = v[j+1];
+	                    v[j+1] = aux;
+	                }
+	            }
+	            //%d = i
+	            //%s = return da funcao arraystostring
+	            //n <br>
+	            System.out.printf("passagem %d -> %s \n",
+	                i, Arrays.toString(v));
+	        }
+	    }    
+	
+	
+}
---
+++ algoritmos2/src/aulas/Web4_InsertionSort.java
@@ -0,0 +1,35 @@
+package aulas;
+import java.util.Arrays;
+
+/*
+ 
+ https://www.youtube.com/watch?v=92BfuxHn2XE
+ */
+
+public class Web4_InsertionSort {
+
+	public static void main(String[] args) {
+		 int[] v= { 32, 17, 51, 8, 23 }; 
+
+	        System.out.printf("inicio -> %s \n", Arrays.toString(v));
+
+	        insertionSort(v);
+
+	        System.out.printf("fim -> %s \n", Arrays.toString(v));
+
+	}
+	
+	public static void insertionSort(int []v) {
+		for (int i = 1; i < v.length; i++ ) { //start com i=1
+			int x = v[i];
+			for (int j = i-1 ; j >= 0 && v[j] > x ; j--) {
+				v[j+1] = v[j];
+				v[j] = x;
+			}
+			System.out.printf("Iteração do  número %d -> %s \n ", x, Arrays.toString(v));
+		}
+	}
+	
+	
+
+}
