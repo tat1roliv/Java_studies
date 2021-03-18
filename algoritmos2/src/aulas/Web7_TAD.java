5c63fd7a297ce0c2d8d236e9dc2a1f4cb1128365
tati oliveira <tat1roliv@gmail.com>
2021-03-18 11:23:55 -0300

    intro POO

---
+++ algoritmos2/src/aulas/Web7_TAD.java
@@ -0,0 +1,51 @@
+package aulas;
+
+
+/*
+TAD (dado abstrato) X TPD (dado primitivo)
+
+construção de novos dados/tipos a partir dos dados primitivos (encapsulamento) -> classe;
+
+classe -> TAD, encapsula (const, var e functions = variaveis de instancia(atributos)) para manipular;
+
+estrutura (class)
+
+public class <nomeClasse>
+
+	//variaveis de instancia
+	private <tipo> <atributos>
+	//para a definição de construtores...
+	public <nomeClasse> (<parametros>)
+		{
+		//corpo construtor
+	}
+	//metodos
+	public <tipo de retorno>
+		<nome do metodo> (<parametros>)
+	{
+	//corpo do metodo
+	}
+	//outros metodos construtores
+	 * 
+	 * 
+construtores;
+atributos;
+setters & getters;
+this -> acessar metodos public do objeto;
+metodos;
+static -> nao precisa de um objeto instanciado (=nao usado na poo);
+modificadores de acesso (public, protected, private);
+
+
+
+ */
+
+
+public class Web7_TAD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
