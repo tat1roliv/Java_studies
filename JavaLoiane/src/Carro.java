ddc59449bcc716a092d90d3544e24bb815f1fc6f
tati oliveira <tat1roliv@gmail.com>
2021-09-02 20:25:38 -0300

    ajustando classe -> this

--- JavaLoiane/src/Carro.java
+++ JavaLoiane/src/Carro.java
@@ -10,36 +10,46 @@ public class Carro {
 	
 
 	//construtores
-	Carro() {
-		System.out.println("a classe carro foi instanciada...");
-		numPassageiros = 4;
+	//boa pratica => quando criar construtores, criar tambem o vazio
+	public Carro() {
 		
 	}
 	
-	Carro (String marca_, String modelo_, double capCombustivel_, int numPassageiros_, double consumoCombustivel_){
-		marca = marca_;
-		modelo = modelo_;
-		numPassageiros = numPassageiros_;
-		capCombustivel = capCombustivel_;
-		consumoCombustivel = consumoCombustivel_;
+	public Carro (String marca, String modelo, double capCombustivel, int numPassageiros, double consumoCombustivel){
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
 
 	}
 	
+	public Carro(String marca, String modelo, int numPassageiros) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		System.out.println("Chamando construtor com 3 parâmetros");
+	}
 	
+	public Carro(String marca, String modelo) {
+		this(marca,modelo, 10);
+		System.out.println("Chamando construtor com 2 parâmetros");
+	}
+
 	//metodos
 	void exibirAutonomia() {
-		System.out.println("A autonomia do carro modelo " + modelo + " é de " + capCombustivel * consumoCombustivel + " km");
+		System.out.println("A autonomia do carro modelo " + this.modelo + " é de " + this.capCombustivel * this.consumoCombustivel + " km");
 		
 	}
 	
 	double ObterAutonomia() {
-		return capCombustivel * consumoCombustivel;
+		return this.capCombustivel * this.consumoCombustivel;
 	}
 	
 	//parametro info que nao tem na classe
 	double calcularCombustivel (double km) {
 		
-		double qtdCombustivel = km / consumoCombustivel;
+		double qtdCombustivel = km / this.consumoCombustivel;
 		return qtdCombustivel;
 	}
 	
---
+++ JavaLoiane/src/TestaCarro3.java
@@ -0,0 +1,9 @@
+
+public class TestaCarro3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
