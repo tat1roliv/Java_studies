89019e75b748c57028827d786d987eea021ab327
tati oliveira <tat1roliv@gmail.com>
2021-09-04 14:11:57 -0300

    pti parte 1

---
+++ pti_algoritmos/src/pti_algoritmos/RecebeLista.java
@@ -0,0 +1,10 @@
+package pti_algoritmos;
+
+public class RecebeLista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pti_algoritmos/src/pti_algoritmos/RecebeVetSomaPares.java
@@ -0,0 +1,48 @@
+package pti_algoritmos;
+import java.util.Scanner;
+
+public class RecebeVetSomaPares {
+
+	public static void main(String[] args) {
+		
+		int nVetor = lerElementosVetor(0);//le qtd posicoes []v
+		int v[] = new int[nVetor];//cria []v com n informado
+		lerVetor(v);//solicita v[i] para todas as posicoes necessarias
+		System.out.print("Vetor declarado: ");
+		imprimir(v);//imprime v declarado
+		int resultado = somaPares(v);
+		System.out.print("Soma dos valores pares informados: "+ resultado);//calcula se par e exibe return da funcao	
+	}
+	
+	public static int lerElementosVetor(int n) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.print("Digite o número de elementos do vetor: ");
+		String nVetor  = leitor.nextLine();
+		return Integer.parseInt(nVetor);	
+	}
+	
+    public static void lerVetor(int []vet){
+        Scanner leitor = new Scanner(System.in);
+        for (int i = 0; i < vet.length; i++){
+        	System.out.print("Informe o elemento na posição V["+i+"]: ");
+        	vet[i] = leitor.nextInt();
+       }
+    }
+  
+    public static void imprimir(int[] vet){  	
+        for (int i = 0; i < vet.length; i++) {
+        System.out.print(vet[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static int somaPares(int []vet) {
+    	int soma = 0;
+    	for (int i = 0; i < vet.length; i++) {
+    		if( vet[i] % 2 == 0) {
+    			soma = soma + vet[i];	
+    		}
+    	}
+    	return soma;
+    }   	
+}
