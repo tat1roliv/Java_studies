3a43cd9ad1d2eb9fd5822c86009d905a5708cd6f
tati oliveira <tat1roliv@gmail.com>
2021-09-04 10:54:39 -0300

    modificador static

--- JavaLoiane/src/MinhaCalculadora.java
+++ JavaLoiane/src/MinhaCalculadora.java
@@ -1,15 +1,16 @@
 
 public class MinhaCalculadora {
 
-	public double soma(double d, double e) {
+	//static => nao precisa da instancia da classe para utilizar o metodo
+	public static double soma(double d, double e) {
 		return d + e;
 	}
 	
-	public double soma(double num1, double num2, double num3) {
+	public static double soma(double num1, double num2, double num3) {
 		return num1 + num2 + num3;
 	}
 	
-	public int soma(int[] vetorInteiros) {
+	public static int soma(int[] vetorInteiros) {
 		int total = 0;
 		
 		for (int i = 0; i < vetorInteiros.length; i++) {
--- JavaLoiane/src/TesteCalculadora.java
+++ JavaLoiane/src/TesteCalculadora.java
@@ -1,15 +1,23 @@
 
 public class TesteCalculadora {
 
+	//static => nao precisa da instancia da classe para utilizar o metodo
+	static int resultadoSoma;
+	
 	public static void main(String[] args) {
-		
+
 		MinhaCalculadora calc = new MinhaCalculadora();
 		
-		calc.soma(1,2);
+		System.out.println(calc.soma(1,2));
 		
-		calc.soma(6.5,7.5);
+		System.out.println(calc.soma(6.5,7.5));
 		
+		System.out.println(soma2Valores(1,2));
 
+	}
+	
+	static double soma2Valores(int num1, int num2) {
+		return MinhaCalculadora.soma(num1, num2);
 	}
 
 }
