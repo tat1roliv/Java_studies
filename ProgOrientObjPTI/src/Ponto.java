71332255535a7349b4643500aa98b436665ee00a
tati oliveira <tat1roliv@gmail.com>
2021-09-04 19:37:00 -0300

    pti

---
+++ ProgOrientObjPTI/src/Ponto.java
@@ -0,0 +1,55 @@
+
+public class Ponto {
+	
+	//ATRIBUTOS (encapsulados)
+	private int coordX;
+	private int coordY;
+	
+	//CONSTRUTORES
+	public Ponto(){
+		this.coordX = 0;
+		this.coordY = 0;
+	}
+
+	public void Ponto(int coordX, int coordY) {
+		this.coordX = coordX;
+		this.coordY = coordY;
+	}
+
+	//METODOS
+	public boolean testaIgualdadePontos(int coordX, int coordY) {
+		if ( this.getCoordX() == coordX && this.getCoordY() == coordY) {
+			return true;
+		}
+		return false;
+	}
+
+	  public boolean testaIgualdadePontos(Ponto ponto) { 
+		  if ( ponto == ponto) { 
+			  return true;
+		  } 
+		  return false; 
+	  }
+	 
+	public double calculaDistanciaPontos(Ponto ponto) {
+		double dist;
+		int coordX = ponto.coordX;
+		int coordY = ponto.coordY;
+		return dist = Math.sqrt((this.coordX-coordX) * (this.coordX-coordX) + (this.coordY-coordY) * (this.coordY-coordY));	 	    
+	}
+	
+	//getters and setters 
+	public int getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+	public int getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+
+}
---
+++ ProgOrientObjPTI/src/TestePonto.java
@@ -0,0 +1,34 @@
+
+public class TestePonto {
+	
+	public static void txtEntradaPrograma() {
+		System.out.println("       P R O G R A M A 	    ");
+		System.out.println("************  PTI  *************");
+		System.out.println("Programação Orientada a Objetos\n");
+		//System.out.println("________________________________\n");
+	}
+
+	public static void main(String[] args) {
+				
+		txtEntradaPrograma();
+		
+		Ponto pontoTeste1 = new Ponto();
+		pontoTeste1.setCoordX(1);
+		pontoTeste1.setCoordY(5);
+		System.out.println("Ponto 1, Coordenada x: " + pontoTeste1.getCoordX() + "\nPonto 1, Coordenada y: " + pontoTeste1.getCoordY());
+		
+		Ponto pontoTeste2 = new Ponto();
+		pontoTeste2.Ponto(1,2);
+		System.out.println("Ponto 2, Coordenada x: " + pontoTeste2.getCoordX() + "\nPonto 2, Coordenada y: " + pontoTeste2.getCoordY());
+		
+		if (pontoTeste1.testaIgualdadePontos(pontoTeste2)) {		
+			System.out.println("Os pontos informados são iguais");
+		}else {
+			System.out.println("Os pontos informados NÃO são iguais");
+		}	
+		
+		System.out.println("Distância entre os pontos: " + pontoTeste1.calculaDistanciaPontos(pontoTeste2));
+		
+	}
+
+}
