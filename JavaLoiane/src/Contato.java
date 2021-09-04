2f9da471edc55d8d851164f26d46c7ccebb726d5
tati oliveira <tat1roliv@gmail.com>
2021-09-04 13:03:56 -0300

    relacionamento entre classes => 1 e *

--- JavaLoiane/src/Contato.java
+++ JavaLoiane/src/Contato.java
@@ -1,9 +1,37 @@
 
 public class Contato {
 	
-	String nome;
-	String email;
-	String endereco;
-	String[] telefones;
+	private String nome;
+	private String email;
+	private Endereco endereco;
+	private Telefone[] telefones;
+
+	
+	//getters && setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Telefone[] getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(Telefone[] telefones) {
+		this.telefones = telefones;
+	}
 
 }
--- JavaLoiane/src/TestaContato.java
+++ JavaLoiane/src/TestaContato.java
@@ -3,16 +3,72 @@ public class TestaContato {
 
 	public static void main(String[] args) {
 		
-		Contato contato0  = new Contato();
-		contato0.nome = "Joao";
-		contato0.endereco = "Av 1, 90";
-		contato0.email = "joao@gmail.cm";
+		Contato contato  = new Contato();
+		contato.setNome("Maria Silva");
+		contato.setEmail("maria@gmail.cm");
 		
-		contato0.telefones = new String[5];
-		contato0.telefones[0] = "111111111";
-		contato0.telefones[1] = "999999999";
+		//contato.setTelefone(new String[5]);
+		//contato.getTelefone()[0] = "111111111";
+		//contato.getTelefone()[1] = "999999999";
 		
-		System.out.println(contato0.telefones[1]);
+		System.out.println(contato.getNome());
+		//System.out.println(contato.getTelefone()[1]);
+		
+		//relacionamento classe contato e classe endereco
+		Endereco end = new Endereco();
+		end.setNomeRua("Rua ABC");
+		end.setNumero("123");
+		end.setComplemento("Apto 1");
+		end.setCep("51110-120");
+		end.setCidade("Hellcife");
+		end.setEstado("PE");
+		
+		contato.setEndereco(end);
+		
+		//java8 => null safe operator
+		if(contato != null && contato.getEndereco() != null) {
+			System.out.println("\nDados Endereço");
+			System.out.println(contato.getEndereco().getNomeRua());
+			System.out.println(contato.getEndereco().getNumero());
+			System.out.println(contato.getEndereco().getComplemento());
+			System.out.println(contato.getEndereco().getCep());
+			System.out.println(contato.getEndereco().getCidade());
+			System.out.println(contato.getEndereco().getEstado());
+		}
+		
+		//relacionamento classe contato e classe telefone
+		Telefone tel1 = new Telefone();
+		tel1.setTipo("fixo");
+		tel1.setDdd("081");
+		tel1.setNumero("32551111");
+		
+		Telefone tel2 = new Telefone();
+		tel2.setTipo("celular");
+		tel2.setDdd("099");
+		tel2.setNumero("99999911");
+		
+		Telefone[] telefones = new Telefone[2];
+		telefones[0] = tel1;
+		telefones[1] = tel2;
+		
+		contato.setTelefones(telefones);
+		
+		if(contato != null && contato.getTelefones() != null) {
+			System.out.println("\nDados Telefones");
+			
+			for (Telefone t : contato.getTelefones()) {
+				System.out.println(t.getTipo());
+				System.out.println(t.getDdd());
+				System.out.println(t.getNumero());
+				
+			}
+			
+		
+		}
+		
+		
+		
+		
 		
 
 	}
