2ded10f262d945e08c280b6500fbb90f2f676277
tati oliveira <tat1roliv@gmail.com>
2021-09-08 21:48:29 -0300

    heranca aula1

--- JavaLoianeOOHeranca/src/Professor.java
+++ JavaLoianeOOHeranca/src/Professor.java
@@ -1,10 +1,6 @@
 
-public class Professor {
+public class Professor extends Pessoa {
 
-	private String nome;
-	private String endereco;
-	private String telefone;
-	private String cpf;
 	private double salario;
 	private String nomeCurso;
 	
@@ -14,10 +10,6 @@ public class Professor {
 	}
 	
 	public Professor(String nome, String endereco, String telefone, String cpf, double salario, String nomeCurso) {
-		this.nome = nome;
-		this.endereco = endereco;
-		this.telefone = telefone;
-		this.cpf = cpf;
 		this.salario = salario;
 		this.nomeCurso = nomeCurso;
 	}
@@ -31,30 +23,6 @@ public class Professor {
 	
 	
 	//getters setters
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
 	public double getSalario() {
 		return salario;
 	}
---
+++ JavaLoianeOOHeranca/src/Teste.java
@@ -0,0 +1,19 @@
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		//acesso a toda a classe aluno
+		
+		aluno.setNome("Maria");
+		
+		Pessoa professor = new Professor();
+		//nessa forma de declaracao, só se tem acesso aos atributos da classe pessoa, sem as particularidades da classe professor
+		
+	}
+	
+	
+
+
+}
